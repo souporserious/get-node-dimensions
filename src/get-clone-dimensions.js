88272b5129015c87f362e80ceb63c87963808f92
Travis Arnold <ftntravis@gmail.com>
2016-10-12 17:47:27 -0700

    allow width and height to be set on clone using options, use offset width/height when measuring clone so transforms don't give inaccurate dimensions

--- src/get-clone-dimensions.js
+++ src/get-clone-dimensions.js
@@ -3,7 +3,7 @@ export default function getCloneDimensions(node, options) {
   const context = document.createElement('div')
   const clone = node.cloneNode(true)
   const style = getComputedStyle(node)
-  let rect = {}
+  let rect, width, height
 
   // give the node some context to measure off of
   // no height and hidden overflow hide node copy
@@ -16,14 +16,14 @@ export default function getCloneDimensions(node, options) {
   clone.setAttribute('name', '')
 
   // set props to get a true dimension calculation
-  if (style.getPropertyValue('display') === 'none') {
+  if (options.display || style.getPropertyValue('display') === 'none') {
     clone.style.display = options.display || 'block'
   }
-  if (!parseInt(style.getPropertyValue('width'))) {
-    clone.style.width = 'auto'
+  if (options.width || !parseInt(style.getPropertyValue('width'))) {
+    clone.style.width = options.width || 'auto'
   }
-  if (!parseInt(style.getPropertyValue('height'))) {
-    clone.style.height = 'auto'
+  if (options.height || !parseInt(style.getPropertyValue('height'))) {
+    clone.style.height = options.height || 'auto'
   }
 
   // append copy to context
@@ -32,11 +32,20 @@ export default function getCloneDimensions(node, options) {
   // append context to DOM so we can measure
   parentNode.appendChild(context)
 
-  // get accurate width and height
+  // get accurate dimensions
   rect = clone.getBoundingClientRect()
+  width = clone.offsetWidth
+  height = clone.offsetHeight
 
   // destroy clone
   parentNode.removeChild(context)
 
-  return rect
+  return {
+    width,
+    height,
+    top: rect.top,
+    right: rect.right,
+    bottom: rect.bottom,
+    left: rect.left,
+  }
 }
--- src/get-node-dimensions.js
+++ src/get-node-dimensions.js
@@ -2,18 +2,14 @@ import getCloneDimensions from './get-clone-dimensions'
 
 export default function getNodeDimensions(node, options = {}) {
   let rect = node.getBoundingClientRect()
-  let width = rect.width
-  let height = rect.height
 
-  if (!width || !height || options.clone) {
+  if (!rect.width || !rect.height || options.clone) {
     rect = getCloneDimensions(node, options)
-    width = rect.width
-    height = rect.height
   }
 
   return {
-    width,
-    height,
+    width: rect.width,
+    height: rect.height,
     top: rect.top,
     right: rect.right,
     bottom: rect.bottom,
